4ef554718f7c8c158d5c3fe4103af5ed536f5671
genkihao <genkihao@tencent.com>
2019-11-25 16:35:59 +0800

    update reporter name

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('reporter/new', views.new_reporter, name='new reporter'),
+    path('reporter/edit', views.edit_reporter, name='edit reporter name'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,6 +14,23 @@ def new_reporter(request):
     exist = Reporter.objects.filter(full_name=full_name)
     if len(exist):
         return HttpResponse("exist name")
-    reporter = Reporter(full_name=get['name'])
+    reporter = Reporter(full_name=full_name)
     reporter.save()
     return HttpResponse("new reporter id is " + str(reporter.id))
+
+
+def edit_reporter(request):
+    get = request.GET
+    if ('id' not in get) or ('name' not in get):
+        return HttpResponse("Invalid param")
+    reporters = Reporter.objects.filter(id=get['id'])
+    if len(reporters) == 0:
+        return HttpResponse("empty set")
+    full_name = get['name']
+    exist = Reporter.objects.filter(full_name=full_name).exclude(id=get['id'])
+    if len(exist):
+        return HttpResponse("exist name")
+    reporter = reporters[0]
+    reporter.full_name = full_name
+    reporter.save()
+    return HttpResponse('OK')
